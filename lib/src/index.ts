2e30b813056925109682475013c6cf318eda6815
jbathen <bathenjared@gmail.com>
2025-08-16 13:10:11 -0500

    added exports from root

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,1 +1,6 @@
-export * from "./assets/Logo";  // add more exports as you build them
+export { PalettePlusProvider } from "./components/palette-plus/Provider";
+export { default as PanelContent } from "./components/palette-plus/PanelContent";
+export { default as PalettePanel } from "./components/palette-plus/Panel";
+export { default as PaletteToggleButton } from "./components/palette-plus/ToggleButton";
+export { default as Palette } from "./components/Palette";
+export { Logo } from "./assets/Logo";
